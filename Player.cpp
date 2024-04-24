e95bfc8270020b7651cd8c5cd481acb139c99742
幸大 畠山 <230169@jc-21.jp>
2024-04-24 12:00:22 +0900

    プレイヤーの移動をなめらか？にした

--- Player.cpp
+++ Player.cpp
@@ -25,18 +25,60 @@ void Player::Initialize()
 void Player::Update()
 {
 
-	if (Input::IsKeyDown(DIK_LEFT))
-	{
-		transform_.position_.x = max(transform_.position_.x - 1.2, -1.2); // ¶‚ÉˆÚ“®
-	}
-	if (Input::IsKeyDown(DIK_RIGHT))
-	{
-		transform_.position_.x = min(transform_.position_.x + 1.2, 1.2); // ‰E‚ÉˆÚ“®
-	}
 
-	BoxCollider* collision = new BoxCollider(XMFLOAT3(0, 0, 0), XMFLOAT3(1, 1, 1));
-	AddCollider(collision);
+    //tank.fbx‚ÌŒü‚«‚ª‹t‚¾‚Á‚½‚©‚ç180“x•Ï‚¦‚Ä‚é
+    static float targetPosition = 0.0f;
+    static float moveAmount = 0.1f;
+    static float targetRotation = 180.0f;
+    static float rotationSpeed = 5.0f;
+
+    if (Input::IsKeyDown(DIK_LEFT) && targetPosition > -1.2f)
+    {
+        targetPosition -= 1.2f;
+        targetRotation = 180.0f - 45.0f;
+    }
+
+    else if (Input::IsKeyDown(DIK_RIGHT) && targetPosition < 1.2f)
+    {
+        targetPosition += 1.2f;
+        targetRotation = 180.0f + 45.0f;
+    }
+
+
+    if (transform_.rotate_.y < targetRotation)
+    {
+        transform_.rotate_.y = min(transform_.rotate_.y + rotationSpeed, targetRotation);
+        if (abs(targetRotation - transform_.rotate_.y) < rotationSpeed)
+        {
+            targetRotation = 180.0f;
+        }
+    }
+    else if (transform_.rotate_.y > targetRotation)
+    {
+        transform_.rotate_.y = max(transform_.rotate_.y - rotationSpeed, targetRotation);
+        if (abs(targetRotation - transform_.rotate_.y) < rotationSpeed)
+        {
+            targetRotation = 180.0f;
+        }
+    }
+
+    if (transform_.position_.x < targetPosition)
+    {
+        transform_.position_.x = min(transform_.position_.x + moveAmount, targetPosition);
+    }
+    else if (transform_.position_.x > targetPosition)
+    {
+        transform_.position_.x = max(transform_.position_.x - moveAmount, targetPosition);
+    }
+
+    BoxCollider* collision = new BoxCollider(XMFLOAT3(0, 0, 0), XMFLOAT3(1, 1, 1));
+    AddCollider(collision);
 }
+
+
+
+
+
 
 
 void Player::Draw()
